a399ed9beeb595671db7071845c483e2ee35be4c
M S Nishaanth <42349645+nishaaanth2@users.noreply.github.com>
2023-11-13 00:20:02 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      // ...
+      animation: {
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
